03d1d868b50615947ea59bd6d6510f2182ea4de4
diproray <diprorayy@gmail.com>
2018-04-20 00:59:35 -0500

    Moving Graph visualization added.

--- final-project-of/src/ofApp.cpp
+++ final-project-of/src/ofApp.cpp
@@ -1,17 +1,33 @@
 #include "ofApp.h"
 
+const int kDefaultTextSize = 20;
+const float kDefaultSongPlaySpeed = 1.0f;
+const float kSpectrumSmoothingFactor = 0.96f;
+
 /**
  * The following function sets up the resources for the application.
  */
 void ofApp::setup(){
-
+    
+    initializeResourcesForMovingGraphVisualizer();
+    text_font_loader_.load("freight-sans.ttf", kDefaultTextSize);
+    sound_player_.load("all_my_love.mp3");
+    sound_player_.play();
+    sound_player_.setLoop(true);
+    sound_player_.setSpeed(kDefaultSongPlaySpeed);
+    
 }
 
 /**
  * The following function updates the resources for the application.
  */
 void ofApp::update(){
-
+    
+    ofSoundUpdate();
+    
+    float * current_sound_spectrum_values = ofSoundGetSpectrum(kNumberOfBands_);
+    updateValuesForMovingGraphVisualizer(current_sound_spectrum_values);;
+    
 }
 
 /**
@@ -19,7 +35,8 @@ void ofApp::update(){
  * application window.
  */
 void ofApp::draw(){
-
+    ofSetBackgroundColorHex(0xC9E9F6); // Set background to turquoise colour.
+    drawEqualizerBarsAndMovingGraph();
 }
 
 /**
@@ -28,6 +45,117 @@ void ofApp::draw(){
  */
 void ofApp::keyPressed(int key){
 
+}
+
+void ofApp::initializeResourcesForMovingGraphVisualizer() {
+    
+    for (int index = 0; index < kNumberOfBands_; index++) {
+        spectrum_values_array_[index] = 0.0;
+    }
+    
+    for (int index = 0; index < kNumberOfParticles_; index++) {
+        
+        particle_offset_x_axis_[index] = ofRandom(0, 2000);
+        particle_offset_y_axis_[index] = ofRandom(0, 2000);
+        
+    }
+    
+    graph_radius_ = 500;
+    
+    particle_velocity_ = 0.1;
+    
+    band_index_for_graph_radius_ = 2;
+    
+    band_index_for_particle_velocity_ = 100;
+    
+    current_time_ = 0;
+    
+}
+
+void ofApp::updateValuesForMovingGraphVisualizer(float * new_spectrum_values) {
+    
+    for ( int index = 0; index < kNumberOfBands_; index++ ) {
+        
+        spectrum_values_array_[index] *= kSpectrumSmoothingFactor;
+        spectrum_values_array_[index] = max(spectrum_values_array_[index], new_spectrum_values[index]);
+        
+    }
+    
+    float new_current_time = ofGetElapsedTimef();
+    float delta_time = new_current_time - current_time_;
+    delta_time = ofClamp(delta_time, 0.0, 0.1);
+    current_time_ = new_current_time;
+    
+    graph_radius_ =
+        ofMap(spectrum_values_array_[band_index_for_graph_radius_], 1, 3, 400, 800, true);
+    
+    particle_velocity_ =
+        ofMap(spectrum_values_array_[band_index_for_particle_velocity_], 0, 0.1, 0.05, 0.5);
+    
+    for (int index = 0; index < kNumberOfParticles_; index++) {
+        
+        particle_offset_x_axis_[index] += particle_velocity_ * delta_time;
+        particle_offset_y_axis_[index] += particle_velocity_ * delta_time;
+        
+        // Calculate Perlin's noise in [-1, 1] and
+        // Multiply it by the graph radius to get the new position of the particle.
+        
+        particles[index].x = ofSignedNoise(particle_offset_x_axis_[index]) * graph_radius_;
+        particles[index].y = ofSignedNoise(particle_offset_y_axis_[index]) * graph_radius_;
+    }
+}
+
+void ofApp::drawEqualizerBarsAndMovingGraph() {
+    
+    ofSetColor(230, 230, 230);
+    ofFill();
+    ofDrawRectangle(10, 700, kNumberOfBands_ * 5.5, - 100);
+    
+    ofSetColor(0, 0, 0);
+    for (int index = 0; index < kNumberOfBands_; index++) {
+        
+        if (index == band_index_for_graph_radius_
+                || index == band_index_for_particle_velocity_) {
+            ofSetColor(0, 0, 0); // Black color
+        }
+        else {
+            ofSetColor(128, 128, 128); // Gray color
+        }
+        
+        ofDrawRectangle(10 + index * 5, 700, 3, -spectrum_values_array_[index] * 200);
+    }
+
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+
+    
+    for (int index = 0; index < kNumberOfParticles_; index++) {
+        
+        ofSetColor(rand() % 255, rand() % 255, rand() % 255);
+        ofFill();
+        ofDrawCircle(particles[index], 2);
+    }
+
+    
+    float threshold_distance = 40;    // Threshold parameter of "close" distance
+    
+    for (int index_one = 0; index_one < kNumberOfParticles_; index_one++) {
+        for (int index_two = index_one + 1; index_two < kNumberOfParticles_; index_two++) {
+            if (ofDist(particles[index_one].x, particles[index_one].y, particles[index_two].x, particles[index_two].y)
+                < threshold_distance) {
+                
+                ofSetColor(rand() % 255, rand() % 255, rand() % 255);
+                ofFill();
+                
+                // ofDrawTriangle(particles[index_one], particles[index_two], particles[index_one + 1]);
+                ofDrawLine(particles[index_one], particles[index_two]);
+            }
+        }
+    }
+    
+    // Revert the coordinate system to original system
+    ofPopMatrix();
 }
 
 
--- final-project-of/src/ofApp.h
+++ final-project-of/src/ofApp.h
@@ -6,7 +6,36 @@
  * The ofApp class that contains all the functions which are to be run.
  */
 class ofApp : public ofBaseApp {
+    
+    ofSoundPlayer sound_player_; // the sound player for the music to be visualized
+    
+    ofTrueTypeFont text_font_loader_; // the TTF file loader for
+                                      // the font the text is to be displayed in
+    
+    static const int kNumberOfBands_ = 256;  // Number of bands in the spectrum
+    
+    float spectrum_values_array_[kNumberOfBands_]; // Array storing
+    // smoothed spectrum float values
+    
+    float graph_radius_;  // Graph radius parameter
+    
+    float particle_velocity_;  // Graph particle velocity parameter
+    
+    int band_index_for_graph_radius_; // Band index in spectrum that affects graph radius value
+    
+    int band_index_for_particle_velocity_; // Band index in spectrum that affects particle velocity value
+    
+    static const int kNumberOfParticles_ = 300; // Number of graph particles
+    
+    float particle_offset_x_axis_[kNumberOfParticles_]; // Array storing offsets for Perlin Noise calculation of particles' x-positions
+    
+    float particle_offset_y_axis_[kNumberOfParticles_]; // Array storing offsets for Perlin Noise calculation of particles' y-positions
+    
+    ofPoint particles[kNumberOfParticles_]; // Moving graph's particles positions
+    
+    float current_time_;        // Time value, used for computing delta (time)
 
+    
 	public:
     
         /**
@@ -30,6 +59,13 @@ class ofApp : public ofBaseApp {
          * that is pressed.
          */
 		void keyPressed(int key);
+    
+        void initializeResourcesForMovingGraphVisualizer();
+    
+        void updateValuesForMovingGraphVisualizer(float * new_spectrum_values);
+    
+        void drawEqualizerBarsAndMovingGraph();
+    
     
         // The below functions are currently not being used.
     
